0ee1f0852d5725331d33373173d0dff31be6ccd7
Tera15 <ccatherall15@gmail.com>
2020-02-23 17:27:31 -0800

    added firebase initialization

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAOq8mgZWz0yyRvYIZHDfGOSsa5mhMoDbA",
+    authDomain: "kc-clothing-6b829.firebaseapp.com",
+    databaseURL: "https://kc-clothing-6b829.firebaseio.com",
+    projectId: "kc-clothing-6b829",
+    storageBucket: "kc-clothing-6b829.appspot.com",
+    messagingSenderId: "449503952466",
+    appId: "1:449503952466:web:f671e5e53e108a8aa4a98b",
+    measurementId: "G-YRSBLDC7R2"
+  }
+
+  firebase.initializeApp(config)
