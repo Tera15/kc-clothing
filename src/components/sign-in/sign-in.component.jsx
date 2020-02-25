e9c91272179d637b0ea7e12d743cad7a7a4441cd
Tera15 <ccatherall15@gmail.com>
2020-02-25 14:53:46 -0800

    integrated firebase, created sign in page with sign in component and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import  './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -47,8 +49,11 @@ class SignIn extends React.Component {
                     required
                     />
                 
-
+                <div className='buttons'>
                     <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
+                    {/*isGoogleSignIn will evaluate to true if no value passed. Help to conditionally render our button*/}
+                </div>
                 </form>
             </div>
         );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,4 +13,14 @@ const config = {
     measurementId: "G-YRSBLDC7R2"
   }
 
-  firebase.initializeApp(config)
+  firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+ 
