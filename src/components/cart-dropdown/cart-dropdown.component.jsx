9a37984007bf2796cbf9859d6317d190d5b78b9b
Tera15 <ccatherall15@gmail.com>
2020-03-04 06:30:24 -0800

    added checkout page, checkout-item component and updated cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-
-const CartDropdown = ({ cartItems }) => (
+// dispatch is passed as a prop to component from connect if connect is not passed a second argument of mapDispatchToProps
+// usefull to not have to write unnecessary mapDispatchToProps
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            {
+            {   
+                cartItems.length ? // if greater than zero render cartItems otherwise render empty message span
                 cartItems.map( cartItem => (
                     <CartItem key={cartItem.id} item={cartItem} />
                 ))
+                :
+                <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={
+            () => { 
+                history.push('/checkout'); 
+                dispatch(toggleCartHidden());
+            }}
+        >GO TO CHECKOUT</CustomButton>
     </div>
 );
 
 //params destructed off of state (nested object destructure)
-const mapStateToProps = state =>({
-   cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown)); //evaluates from inside out
+                                                                    // higher order component withRouter() taking:
+                                                                    // the HoC connect(mapStateToProps)(CartDropdown) as an argument
---
+++ src/redux/user/user.selectors.js
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect';
+
+//input selector
+const selectUser = state => state.user;
+
+
+export const selectCurrentUser = createSelector(
+    // array of input selectors. alternatively can be passed in as arguments.
+[selectUser], 
+(user) => user.currentUser
+);
