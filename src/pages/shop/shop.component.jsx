c37605baa30e34bb006c5734be3fbfb379af1249
Tera15 <ccatherall15@gmail.com>
2020-03-10 15:18:56 -0700

    moved shop data state out of React and into Redux, connected shop redux state with Shop page component on front end

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
+
+
+
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-           collections: SHOP_DATA
-
-        };
-    }
-
-    render(){
-        const {collections} = this.state;
-        return(
+const ShopPage = ({ collections }) =>  (
             <div className='shop-page'>
                 {
                     collections.map(({id, ...otherCollectionProps}) => (
@@ -24,7 +21,9 @@ class ShopPage extends React.Component {
                 }
             </div>
         );
-    }
-}
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
