6aefa4419ad3253a079ac2bd6fb884388658fa27
Tera15 <ccatherall15@gmail.com>
2020-04-20 11:49:03 -0700

    commented out contact page link while not in use

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -22,9 +22,9 @@ const Header = ({ currentUser, hidden, signOutStart }) => (
             <OptionLink to="/shop">
                 SHOP
             </OptionLink>
-            <OptionLink to="/shop">
+            {/* <OptionLink to="/shop">
                 CONTACT
-            </OptionLink>
+            </OptionLink> */}
             {
                 currentUser ? (
                 <OptionLink as='div' onClick={signOutStart}>
